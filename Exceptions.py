81bd75f32f8c2e91a315f9f09cda75a0d06aaaa9
Infrae <info@infrae.com>
2003-04-28 09:56:49 +0000

    - adding exceptions module, and type specific metadata runtime data initialization.

---
+++ Exceptions.py
@@ -0,0 +1,7 @@
+"""
+author: kapil thangavelu <k_vertigo@objectrealms.net>
+"""
+class NoContext(Exception): pass
+class ConfigurationError(Exception): pass
+class NotFound(Exception): pass
+class CompatiblityException(Exception): pass
---
+++ Initialize.py
@@ -0,0 +1,24 @@
+"""
+Type Specific Initialization of Metadata Binding Runtime Data
+not really meant for public api exposure (certainly not ttw)
+
+Handler Signature is as follows
+
+def metadata_initialization()
+
+author: kapil thangavelu <k_vertigo@objectrealms.net>
+"""
+from Compatiblity import getContentType
+
+_typeHandlers = {}
+
+def registerHandler(content_type, handler):
+    assert iscallable(handler)
+    global _typeHandlers
+    _typeHandlers[content_type]=handler
+
+def getHandler(content):
+    ct = getContentType(content)
+    return _typeHandlers.get(ct)
+
+
