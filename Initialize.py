8550afcd63bc0e857ead93c43d12e63fd9c06b23
Infrae <info@infrae.com>
2003-05-04 20:17:15 +0000

    - initializer handler registry is now qualified, will likely be folded   in with handlers for access.

--- Initialize.py
+++ Initialize.py
@@ -8,17 +8,18 @@ def metadata_initialization(content, bind_data)
 
 author: kapil thangavelu <k_vertigo@objectrealms.net>
 """
+
 from Compatibility import getContentType
 
-_typeHandlers = {}
+_typeInitHandlers = {}
 
 def registerHandler(content_type, handler):
     assert iscallable(handler)
-    global _typeHandlers
-    _typeHandlers[content_type]=handler
+    global _typeInitHandlers
+    _typeInitHandlers[content_type]=handler
 
 def getHandler(content):
     ct = getContentType(content)
-    return _typeHandlers.get(ct)
+    return _typeInitHandlers.get(ct)
 
 
