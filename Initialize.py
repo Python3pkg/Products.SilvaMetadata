eaf546964f66e29ae79bc24d3adfe24d7ecc36d7
Infrae <info@infrae.com>
2003-05-22 16:46:19 +0000

    - add ability to set a default initializer

--- Initialize.py
+++ Initialize.py
@@ -11,15 +11,28 @@ author: kapil thangavelu <k_vertigo@objectrealms.net>
 
 from Compatibility import getContentType
 
+_default_initializer = None
+
 _typeInitHandlers = {}
 
 def registerInitHandler(content_type, handler):
     assert callable(handler)
     global _typeInitHandlers
-    _typeInitHandlers[content_type]=handler
+    
+    if content_type is None:
+        _default_initializer = handler
+    else:
+        _typeInitHandlers[content_type]=handler
 
 def getHandler(content):
     ct = getContentType(content)
-    return _typeInitHandlers.get(ct)
+    
+    handler = _typeInitHandlers.get(ct)
+    
+    if handler is None and _default_initializer:
+        return _default_initializer
+
+    # possibly none
+    return handler
 
 
