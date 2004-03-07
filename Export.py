f4185b08ba831ea6aa3045bbf61ab69f4e04ce7b
Infrae <info@infrae.com>
2004-03-07 15:05:06 +0000

    - in python2.3, True and False have string values of 'True' and 'False'    which breaks import/export roundtrip. Changed serialization to their    integer values.

--- Export.py
+++ Export.py
@@ -41,10 +41,10 @@ class MetadataSetExporter:
 
             print >> out, '  <metadata_element id="%s">' % e.getId()
             print >> out, '   <index_type>%s</index_type>' % e.index_type
-            print >> out, '   <index_p>%s</index_p>' % e.index_p
+            print >> out, '   <index_p>%d</index_p>' % e.index_p
             print >> out, '   <field_type>%s</field_type>' % e.field_type
-            print >> out, '   <acquire_p>%s</acquire_p>' % e.acquire_p
-            print >> out, '   <read_only_p>%s</read_only_p>' % e.read_only_p
+            print >> out, '   <acquire_p>%d</acquire_p>' % e.acquire_p
+            print >> out, '   <read_only_p>%d</read_only_p>' % e.read_only_p
 
             g = e.read_guard
 
