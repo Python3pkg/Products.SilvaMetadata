c4ed70193484d8483cc28769452dfd58783afc0f
Sylvain Viollon <thefunny@gmail.com>
2012-08-06 17:31:34 +0200

    BBB update.

--- Products/SilvaMetadata/__init__.py
+++ Products/SilvaMetadata/__init__.py
@@ -4,6 +4,6 @@ Author: kapil thangavelu <k_vertigo@objectrealms.net>
 
 from silva.core import conf as silvaconf
 
-silvaconf.extensionName('SilvaMetadata')
-silvaconf.extensionTitle('Silva Metadata')
-silvaconf.extensionSystem()
+silvaconf.extension_name('SilvaMetadata')
+silvaconf.extension_title('Silva Metadata')
+silvaconf.extension_system()
